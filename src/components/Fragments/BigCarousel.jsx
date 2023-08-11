ad1477e5df7bf31d256f2d9f1ed6194b67239c8a
NazalPrastya <nazalprastya@gmail.com>
2023-08-11 19:15:17 +0700

    nazal

--- src/components/Fragments/BigCarousel.jsx
+++ src/components/Fragments/BigCarousel.jsx
@@ -2,6 +2,7 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { useEffect, useState } from 'react';
 import { getUpcoming } from '../../services/upcoming.service';
+import { Link } from 'react-router-dom';
 
 const BigCarousel = () => {
   const [upcomings, setUpcomings] = useState([]);
@@ -52,12 +53,12 @@ const BigCarousel = () => {
                 <p className=" text-white text-5xl font-bold  mt-5 mb-5">{upcoming.title}</p>
                 <span className="text-white bg-blue-600 px-3 py-1 text-xs rounded-full mb-6">UpComing Movies</span>
                 <div className="flex mt-6">
-                  <a
-                    href={`/detail/${upcoming.id}`}
+                  <Link
+                    to={`/detail/${upcoming.id}`}
                     className="px-8 py-3 bg-red-600 text-xl mr-5 text-white font-bold rounded hover:bg-white hover:text-red-600 hover:border-red-600 hover:ring-red-600 duration-300 hover:shadow-white hover:shadow-md"
                   >
                     See Detail
-                  </a>
+                  </Link>
                   <div className="flex flex-col text-white">
                     <p>Realease : {formatDate(upcoming.release_date)}</p>
                     <p>
