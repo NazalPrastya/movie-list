ec4ce727a7b7a69ecad734e51ceb3021d61a54fc
NazalPrastya <nazalprastya@gmail.com>
2023-08-12 18:24:23 +0700

    nazal

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <LandingPage />,
-    // errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/detail/:id',
