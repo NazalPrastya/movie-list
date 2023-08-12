dd37083ad47513840fa970370bf6f30db3307ae5
NazalPrastya <nazalprastya@gmail.com>
2023-08-12 16:56:22 +0700

    nazal

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div className="react flex items-center justify-center">
+      <h1 className="text-white">Error Page</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
